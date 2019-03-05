76c2facdd94504803d0cee109deae6ac15211f00
UramnOIL <uramnoil@outlook.com>
2019-03-05 18:05:54 +0900

    kotlinぽい書き方

--- src/main/kotlin/com/uramnoil/nukkit/helloworld/HelloWorld.kt
+++ src/main/kotlin/com/uramnoil/nukkit/helloworld/HelloWorld.kt
@@ -1,12 +1,33 @@
 package com.uramnoil.nukkit.helloworld
 
+import cn.nukkit.Player
+import cn.nukkit.event.EventHandler
+import cn.nukkit.event.Listener
+import cn.nukkit.event.player.PlayerJoinEvent
 import cn.nukkit.plugin.PluginBase
-import com.uramnoil.nukkit.helloworld.event.MyHandler
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class HelloWorld : PluginBase() {
-    private val controller = HelloWorldImpl()
-    private val handler = MyHandler(controller)
+class HelloWorld : PluginBase(), Listener {
+    lateinit var message: String
     override fun onEnable() {
-        server.pluginManager.registerEvents(handler, this)
+        server.pluginManager.registerEvents(this, this)
+        message = "Hello, World"
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        val player = event.player
+        sendMessage(player, 10) {
+            message
+        }
+    }
+
+    fun sendMessage(player: Player, times: Int, proc: () -> String ) = GlobalScope.launch {
+        repeat(times) {
+            delay(1000L)
+            player.sendMessage(proc())
+        }
     }
 }
--- src/main/kotlin/com/uramnoil/nukkit/helloworld/IHelloWorld.kt
+++
@@ -1,7 +0,0 @@
-package com.uramnoil.nukkit.helloworld
-
-import cn.nukkit.Player
-
-interface IHelloWorld {
-    fun sendMessage(player: Player, times: Int = 0, message: String = "Hello, World")
-}
